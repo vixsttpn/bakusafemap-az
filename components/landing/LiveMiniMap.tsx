c59d427355ba475fe1593d0967b476f22bf2275b
vixsttpn <mbgkzw@gmail.com>
2026-09-13 20:55:29 +0400

    Update LiveMiniMap.tsx

--- components/landing/LiveMiniMap.tsx
+++ components/landing/LiveMiniMap.tsx
@@ -3,6 +3,14 @@ import { useEffect, useRef } from 'react';
 import 'maplibre-gl/dist/maplibre-gl.css';
 export default function LiveMiniMap(){
   const ref=useRef<HTMLDivElement>(null);
-  useEffect(()=>{ let map:any; (async()=>{ const ml=await import('maplibre-gl'); if(!ref.current) return; map=new ml.Map({container:ref.current,style:'https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json',center:[49.854,40.376],zoom:11,attributionControl:false}); map.on('load',()=>{ map.getStyle().layers.forEach((l:any)=>{ if(l.type==='line'&&/road|highway|street/.test(l.id)){ try{map.setPaintProperty(l.id,'line-color','#8a8a8a');}catch{}} }); }); })(); return()=>map?.remove(); },[]);
+  useEffect(()=>{
+    let map:any;
+    (async()=>{
+      const ml=await import('maplibre-gl');
+      if(!ref.current) return;
+      map=new ml.Map({container:ref.current,style:'https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json',center:[49.854,40.376],zoom:11,attributionControl:false});
+    })();
+    return()=>map?.remove();
+  },[]);
   return <div ref={ref} className="w-full h- rounded- overflow-hidden" />;
 }
